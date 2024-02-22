9f162038be6290df70dd9882381ef16a37d51dd0
Michael McGee <mmcgee@jumptrading.com>
2024-02-22 22:13:12 +0000

    firedancer: send stake weights to firedancer
    The leader schedule cache is updated so that it has an optional path to
    send stake weights to firedancer.  This is done on a mcache because we
    do not want to drop or miss any recomputations, otherwise Firedancer
    will not recover.  The pack tile uses stake weights to compute the
    leader schedule, and the shred tile uses stake weights to compute shred
    destinations.

--- ledger/src/bank_forks_utils.rs
+++ ledger/src/bank_forks_utils.rs
@@ -98,6 +98,8 @@ pub fn load(
         entry_notification_sender,
         accounts_update_notifier,
         exit,
+        // FIREDANCER: No need to communicate these leader schedules to Firedancer
+        false,
     )?;
     blockstore_processor::process_blockstore_from_root(
         blockstore,
@@ -124,6 +126,8 @@ pub fn load_bank_forks(
     entry_notification_sender: Option<&EntryNotifierSender>,
     accounts_update_notifier: Option<AccountsUpdateNotifier>,
     exit: Arc<AtomicBool>,
+    // FIREDANCER: Pass application name through so leader schedule cache can find the shared workspaces.
+    firedancer_send: bool,
 ) -> LoadResult {
     fn get_snapshots_to_load(
         snapshot_config: &SnapshotConfig,
@@ -203,8 +207,12 @@ pub fn load_bank_forks(
             (bank_forks, None)
         };
 
+    // FIREDANCER: Send leader schedule updates from this cache.
+    // let mut leader_schedule_cache =
+    //     LeaderScheduleCache::new_from_bank(&bank_forks.read().unwrap().root_bank());
+    let bank = &bank_forks.read().unwrap().root_bank();
     let mut leader_schedule_cache =
-        LeaderScheduleCache::new_from_bank(&bank_forks.read().unwrap().root_bank());
+        LeaderScheduleCache::new(bank.epoch_schedule().clone(), bank, firedancer_send);
     if process_options.full_leader_cache {
         leader_schedule_cache.set_max_schedules(usize::MAX);
     }
