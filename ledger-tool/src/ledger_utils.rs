18efdaa64e955d3d30d3d8cbd279db203f959729
Philip Taffet <phtaffet@jumptrading.com>
2024-08-29 20:46:12 +0000

    firedancer: support disabling status cache for benchmarking

--- ledger-tool/src/ledger_utils.rs
+++ ledger-tool/src/ledger_utils.rs
@@ -61,6 +61,8 @@ extern "C" fn fd_ext_poh_publish_leader_schedule( _data: *const u8, _len: u64 ) 
 extern "C" fn fd_ext_larger_max_cost_per_block() -> i32 { 0 }
 #[no_mangle]
 extern "C" fn fd_ext_larger_shred_limits_per_block() -> i32 { 0 }
+#[no_mangle]
+extern "C" fn fd_ext_disable_status_cache() -> i32 { 0 }
 
 pub struct LoadAndProcessLedgerOutput {
     pub bank_forks: Arc<RwLock<BankForks>>,
